cea8389b388e4ab98883a10f43f04ded158f69ee
Deep5656 <51234266+Deep5656@users.noreply.github.com>
2020-04-30 08:51:16 +0530

    added all modification

---
+++ project.js
@@ -0,0 +1,12 @@
+console.log('deepanshu katiyar');
+let div = document.getElementById('boxes');
+let footer = document.querySelector('footer');
+let divHeight = div.clientHeight;
+let footerPosition = footer.offsetHeight;
+if(2===2){
+    // footerPosition=divHeight;
+    footer.setAttribute('class','footerclass');
+}
+let footerclass = Element.querySelector('.footerclass');
+
+console.log(div,footer,divHeight,footerPosition,);
