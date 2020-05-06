6f4f87d24cb7d36a1074e4d97bf8fffacfffcf9e
Deep5656 <51234266+Deep5656@users.noreply.github.com>
2020-05-06 12:22:25 +0530

    removed logo when window size is less than 768px

--- project.js
+++ project.js
@@ -2,4 +2,8 @@ let navbtn = document.getElementById('navbtn');
 navbtn.style = 'font-size:30px;cursor:pointer;position:absolute;top:25px';
 let logo = document.querySelector('.logo');
 logo.style = 'height: 40px; border-radius:50%; position: absolute;top: 32px;left: 105px'
-console.log(logo);
+// console.log(logo);
+if(document.body.clientWidth<768){
+    logo.style = 'height:0px;width:0px';
+    console.log('deep');
+}
