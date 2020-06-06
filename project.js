543d1e9afb7fe0c0df88648d305f5820c1599e80
Deep5656 <51234266+Deep5656@users.noreply.github.com>
2020-06-06 21:28:43 +0530

    updated bug in mobile view of the project.html

--- project.js
+++ project.js
@@ -1,13 +1,14 @@
 //for menubtn....
 
 let navbtn = document.getElementById('navbtn');
+let canvasView = document.getElementById('canvas')
+
 navbtn.style = 'font-size:30px;cursor:pointer;position:absolute;top:26px;left:15px';
 
 if(document.body.clientWidth<768){
 
     // console.log('deep');
     //for canvas in mobile view.
-    let canvasView = document.getElementById('canvas')
     canvasView.style = 'display:none';
     console.log(canvasView); 
 }
