302151219ee37051e49de8766aa188a4fe1c0a9e
Deep5656 <51234266+Deep5656@users.noreply.github.com>
2020-06-06 16:49:53 +0530

    changed size of the calculator in mobile view

--- project.js
+++ project.js
@@ -3,13 +3,8 @@
 let navbtn = document.getElementById('navbtn');
 navbtn.style = 'font-size:30px;cursor:pointer;position:absolute;top:26px;left:15px';
 
-//for logo...
-
-let logo = document.querySelector('.logo');
-logo.style = 'height: 40px; border-radius:50%; position: absolute;top: 32px;left: 105px'
-// console.log(logo);
 if(document.body.clientWidth<768){
-    logo.style = 'height:0px;width:0px';
+
     // console.log('deep');
     //for canvas in mobile view.
     let canvasView = document.getElementById('canvas')
