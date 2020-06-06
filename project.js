81ef1ac919d97db57055c5de7cf7fb7373446cdc
Deep5656 <51234266+Deep5656@users.noreply.github.com>
2020-06-06 21:53:42 +0530

    finally finised all the problem of the canvas problem

--- project.js
+++ project.js
@@ -4,14 +4,6 @@ let navbtn = document.getElementById('navbtn');
 let canvasView = document.getElementById('canvas')
 
 navbtn.style = 'font-size:30px;cursor:pointer;position:absolute;top:26px;left:15px';
-
-if(document.body.clientWidth<768){
-
-    // console.log('deep');
-    //for canvas in mobile view.
-    canvasView.style = 'display:none';
-    console.log(canvasView); 
-}
 canvasView.style='position:fixed;top:100px'
 
 
